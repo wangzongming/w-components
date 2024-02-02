a16cf97e77115bf35ebbcc2ba84c168598b3e717
wangzongming <wangzm@bjzxkj.com>
2024-02-02 11:32:11 +0800

    新增录音组件

--- README.md
+++ README.md
@@ -1,2 +1,111 @@
 # w-components
 微信小程序高质量功能性组件：录音组件、语音播放组件
+
+所提供的的组件不依赖任何第三方组件，下载就能用。
+
+# 组件
+1. 录音组件 √
+2. 正在添加...
+
+
+# 安装 & 引用
+
+1、下载代码
+
+	git clone https://github.com/wangzongming/w-components
+
+2、复制到下程序项目中
+
+3、在 app.json 或者页面的 index.json 中引用要用的组件
+	
+	"usingComponents": {  
+		"w-record": "./w-components/record/index"
+	},
+
+
+# 各组件文档
+## 1. 录音组件
+录音组件提供：开始录音、结束录音、暂停录音、继续录音、重录、回放、暂停回放、继续回放 功能
+
+并且提供高度样式自定义
+
+### 1.1 预览
+
+<img src="./preview/recoed.png" width="100%"  />"
+
+
+### 1.2 调用
+简单使用的场景下只需要一行代码即可
+
+	<w-record bind:onEnd="recordonEnd"></w-record>
+
+### 1.2 配置属性
+
+
+配置属性都是可选的
+
+	// 使用案例	
+	<w-record texts="{{wRecordText}}"></w-record>
+
+	// 详细属性
+
+	// 可以替换默认微信录音设置
+	recorderManagerOptions: {
+		// duration: 600000,
+		duration: 1000 * 10,
+		sampleRate: 44100,
+		numberOfChannels: 1,
+		format: 'mp3',
+		frameSize: 50,
+		encodeBitRate: 64000,
+	},
+
+	// 文本
+	texts: { 
+		start: "开始录音",
+		stop: "结束录音",
+		continue: "继续录音",
+		reset: "重录",
+		play: "回放",
+		playPause: "暂停",
+		playContinue: "继续",
+	},
+
+	// 按钮背景颜色
+	btnBgs: { 
+		start: "linear-gradient(to bottom,#59c574 , #67d467, #67d467)",
+		stop: "linear-gradient(to bottom,#4c7ef3 , #589af8, #589af8)",
+		pause: "#fff",
+		pauseBorder: "#59c574",
+		continue: "#fff",
+		reset: "linear-gradient(to bottom,#59c574 , #67d467, #67d467)",
+		play: "linear-gradient(to bottom,#4c7ef3 , #589af8, #589af8)",
+	}, 
+
+### 1.2 回调
+
+recordonEnd 录音结束后的回调， 回调中提供了本地录音文件的地址
+
+onResume    重录回调
+ 
+	<w-record bind:onEnd="recordonEnd" bind:onResume="onResume"></w-record>
+
+### 1.3 方法
+
+	// 标签设置 id
+	<w-record id="w-record"></w-record>
+
+	// 获取实例并调用方法
+	const wRecord = this.selectComponent('#w-record') 
+
+	
+	// 开始录音
+	wRecord.startRecord();
+	// 结束录音
+	wRecord.stop();
+ 	// 播放录音
+	wRecord.play();
+ 	// 重置到 ready(一开始的状态) 状态
+	wRecord.toReady();
+
+	  
---
+++ record/index.js
@@ -0,0 +1,366 @@
+ Component({
+ 	/**
+ 	 * 组件的属性列表
+ 	 */
+ 	properties: {
+ 		// 可以替换默认微信录音设置
+ 		recorderManagerOptions: Object,
+ 		// 文本
+ 		texts: Object,
+ 		// 按钮背景颜色
+ 		btnBgs: Object, 
+ 	},
+
+ 	/**
+ 	 * 组件的初始数据
+ 	 */
+ 	data: {
+ 		// 录音状态
+ 		recordStatus: "ready", // "ready" | "ing" |  "pause" | "stop" 
+ 		// 音频地址-本地地址
+ 		audioSrc: "",
+ 		// 是否正在播放录音
+ 		playStatus: "ready", // "ready" | "ing" |  "pause" 
+ 		// 已经录制了多少 ms
+ 		recordedMs: 0,
+ 		recordedMsText: "0′ 0″",
+ 		// 最长可以录制多少秒，实际上就是 _recorderManagerOptions.duration
+ 		recordDuration: 0,
+
+ 		// 录音器配置
+ 		_recorderManagerOptions: {},
+ 		defaultRecorderManagerOptions: {
+ 			duration: 600000,
+ 			// duration: 1000 * 10,
+ 			sampleRate: 44100,
+ 			numberOfChannels: 1,
+ 			format: 'mp3',
+ 			frameSize: 50,
+ 			encodeBitRate: 64000,
+ 		},
+
+ 		// 所有按钮背景颜色
+ 		_btnBgs: {},
+ 		// 默认背景颜色
+ 		defaultBtnBgs: {
+ 			start: "linear-gradient(to bottom,#59c574 , #67d467, #67d467)",
+ 			stop: "linear-gradient(to bottom,#4c7ef3 , #589af8, #589af8)",
+ 			pause: "#fff",
+ 			pauseBorder: "#59c574",
+ 			continue: "#fff",
+ 			reset: "linear-gradient(to bottom,#59c574 , #67d467, #67d467)",
+ 			play: "linear-gradient(to bottom,#4c7ef3 , #589af8, #589af8)",
+ 		},
+ 		// 所有文字 默认+传入文字进行合并
+ 		_text: {},
+ 		// 默认文字
+ 		defaultTexts: {
+ 			start: "开始录音",
+ 			stop: "结束录音",
+ 			continue: "继续录音",
+ 			reset: "重录",
+ 			play: "回放",
+ 			playPause: "暂停",
+ 			playContinue: "继续",
+ 		},
+ 	},
+ 	/**
+ 	 * 录音实例
+ 	 */
+ 	recorderManager: null,
+ 	/**
+ 	 * 倒计时圆圈 ctx
+ 	 */
+ 	ctx: null,
+ 	/**
+ 	 * 组件的方法列表
+ 	 */
+ 	methods: {
+ 		// 开始录音
+ 		async start() {
+ 			if (this.data.recordStatus === "ing") {
+ 				// console.log('正在录音中')
+ 				return;
+ 			}
+ 			this.audioIns && this.audioIns.stop();
+ 			// console.log('开始录音')
+ 			const startedFn = () => {
+ 				this.setData({
+ 					recordStatus: "ing",
+ 					recordedMs: 0,
+ 					recordedMsText: "0′ 0″",
+ 					recordDuration: this.data._recorderManagerOptions.duration,
+ 				}, () => {
+ 					// 开始计时动画
+ 					this.jsFn();
+ 					this.progressInit();
+ 				})
+ 			}
+
+ 			const recorderManager = wx.getRecorderManager()
+ 			recorderManager.onStart(() => {
+ 				// console.log('recorder start')
+ 				startedFn();
+ 			})
+ 			recorderManager.onPause(() => {
+ 				// console.log('recorder pause')
+ 				this.setData({
+ 					recordStatus: "pause"
+ 				})
+ 				// 停止计时动画
+ 				clearInterval(this.djsTimer)
+ 			})
+ 			recorderManager.onResume(() => {
+ 				// console.log('recorder onResume') 
+ 				this.setData({
+ 					recordStatus: "ing"
+ 				})
+ 				// 恢复计时动画
+ 				this.jsFn();
+ 				this.progressInit();
+ 			})
+
+ 			recorderManager.onStop((res) => {
+ 				// console.log('recorder stop', res)
+ 				const {
+ 					tempFilePath
+ 				} = res
+ 				// 停止计时动画
+ 				clearInterval(this.djsTimer)
+
+ 				this.setData({
+ 					recordStatus: "stop",
+ 					audioSrc: tempFilePath
+ 				})
+ 				// 触发结束回调
+ 				this.triggerEvent('onEnd', {
+ 					tempFilePath: tempFilePath
+ 				})
+ 			})
+
+ 			recorderManager.onError(({
+ 				errMsg
+ 			}) => {
+ 				// console.log('recorder error', errMsg)
+ 				wx.showToast({
+ 					icon: "error",
+ 					duration: 5000,
+ 					title: `录音错误： ${errMsg}`,
+ 				})
+ 			})
+
+ 			const options = {
+ 				...this.data._recorderManagerOptions
+ 			}
+ 			recorderManager.start(options)
+ 			this.recorderManager = recorderManager;
+ 		},
+ 		// 暂停录音
+ 		async pause() {
+ 			// console.log('暂停录音')
+ 			this.recorderManager.pause()
+ 		},
+ 		// 停止录音
+ 		async stop() {
+ 			// console.log('停止录音')
+ 			if (!this.recorderManager) return;
+ 			await this.recorderManager.stop();
+ 		},
+ 		// 继续录音
+ 		async continue () {
+ 			// console.log('继续录音')
+ 			await this.recorderManager.resume();
+ 		},
+
+ 		// 播放录音
+ 		async play() {
+ 			// console.log('播放录音')
+ 			this.audioIns = this.audioFns();
+ 			this.audioIns.init();
+ 		},
+ 		async playPause() {
+ 			// console.log('暂停播放录音')
+ 			this.audioIns.pause();
+ 		},
+ 		async playResume() {
+ 			// console.log('暂停播放继续')
+ 			this.audioIns.resume();
+ 		},
+ 		// 重录
+ 		async reset() {
+ 			// console.log('重录')
+ 			this.start()
+ 			// 触发重录回调
+ 			this.triggerEvent('onResume', { })
+ 		},
+
+ 		// 计时
+ 		jsFn() {
+ 			clearInterval(this.djsTimer)
+ 			this.djsTimer = setInterval(() => {
+ 				if (this.data.recordedMs >= this.data.recordDuration) {
+ 					clearInterval(this.djsTimer)
+ 					return;
+ 				}
+
+ 				const curMsTime = this.data.recordedMs + 1000;
+ 				const curSTime = parseInt(curMsTime / 1000 % 60);
+ 				const curMTime = parseInt(curMsTime / 1000 / 60);
+ 				// 百分比
+ 				const percent = curMsTime / this.data.recordDuration;
+ 				// 百分比转角度
+ 				const deg = percent * 360;
+ 				// 绘制弧度
+ 				this.progressUpdate(deg);
+ 				this.setData({
+ 					recordedMs: curMsTime,
+ 					recordedMsText: `${curMTime}′ ${curSTime}″`
+ 				})
+ 			}, 1000)
+ 		},
+ 		// 进度条绘制函数
+ 		progressInit() {
+ 			// 绘制进度条 
+ 			wx.createSelectorQuery().in(this).select('#time-canvas')
+ 				.fields({
+ 					node: true,
+ 					size: true
+ 				})
+ 				.exec((res) => {
+ 					if (!res[0]) return;
+ 					const canvas = res[0].node
+ 					const ctx = canvas.getContext('2d')
+ 					const dpr = wx.getSystemInfoSync().pixelRatio;
+ 					const o_size = res[0].width;
+ 					const size = o_size * dpr;
+ 					this.ctx = ctx;
+ 					this.ctxSize = o_size;
+
+ 					this.inited = true;
+ 					canvas.width = size;
+ 					canvas.height = size;
+ 					ctx.scale(dpr, dpr);
+ 					this.data.recordedMs && this.progressUpdate(this.data.recordedMs / this.data.recordDuration * 360)
+ 				})
+ 		},
+ 		// 需要不断更新 
+ 		progressUpdate(deg) {
+ 			const ctx = this.ctx,
+ 				o_size = this.ctxSize;
+ 			if (!ctx) return;
+ 			const pcDeg = (Math.PI / 180) * 90;
+ 			const sDeg = 0 - pcDeg;
+ 			const eDeg = (Math.PI / 180) * deg - pcDeg;
+ 			ctx.clearRect(0, 0, this.ctxSize, this.ctxSize); //清空所有的内容
+ 			ctx.strokeStyle = this.data._btnBgs.pauseBorder
+ 			ctx.lineWidth = 1;
+ 			ctx.beginPath();
+ 			ctx.arc(o_size / 2, o_size / 2, o_size / 2 - 1, sDeg, eDeg);
+ 			ctx.stroke();
+ 		},
+ 		// 语音播放操作函数
+ 		audioFns() {
+ 			const _this = this;
+ 			let innerAudioContext;
+ 			return {
+ 				init() {
+ 					innerAudioContext = wx.createInnerAudioContext({
+ 						useWebAudioImplement: true // 是否使用 WebAudio 作为底层音频驱动，默认关闭。对于短音频、播放频繁的音频建议开启此选项，开启后将获得更优的性能表现。由于开启此选项后也会带来一定的内存增长，因此对于长音频建议关闭此选项
+ 					})
+ 					innerAudioContext.src = _this.data.audioSrc
+ 					innerAudioContext.play() // 播放
+ 					innerAudioContext.onEnded(() => {
+ 						// 播放完毕
+ 						_this.setData({
+ 							playStatus: "ready"
+ 						})
+ 					})
+ 					_this.setData({
+ 						playStatus: "ing"
+ 					})
+ 				},
+ 				async pause() {
+ 					await innerAudioContext.pause()
+ 					return new Promise(async (resolve) => {
+ 						_this.setData({
+ 							playStatus: "pause"
+ 						}, () => resolve())
+ 					})
+ 				},
+ 				async resume() {
+ 					await innerAudioContext.play()
+ 					return new Promise(async (resolve) => {
+ 						_this.setData({
+ 							playStatus: "ing"
+ 						}, () => resolve())
+ 					})
+ 				},
+ 				stop() {
+ 					if (!innerAudioContext) return;
+ 					return new Promise(async (resolve) => {
+ 						innerAudioContext && await innerAudioContext.stop() // 暂停
+ 						_this.setData({
+ 							playStatus: "ready"
+ 						}, () => resolve())
+ 					})
+ 				},
+ 				async destroy() {
+ 					innerAudioContext && innerAudioContext.destroy && await innerAudioContext.destroy() // 释放音频资源
+ 				}
+ 			}
+ 		},
+ 	},
+
+ 	/**
+ 	 * 组件所在页面的生命周期函数
+ 	 */
+ 	lifetimes: {
+ 		attached() {
+ 			// 在组件实例进入页面节点树时执行 
+ 			this.setData({
+ 				_recorderManagerOptions: {
+ 					...this.data.defaultRecorderManagerOptions,
+ 					...this.data.recorderManagerOptions,
+ 				},
+ 				_text: {
+ 					...this.data.defaultTexts,
+ 					...this.data.texts
+ 				},
+ 				_btnBgs: {
+ 					...this.data.defaultBtnBgs,
+ 					...this.data.btnBgs
+ 				}
+ 			})
+ 		},
+ 		detached() {
+ 			// 在组件实例被从页面节点树移除时执行
+ 			this.audioIns && this.audioIns.stop();
+ 			this.recorderManager && this.recorderManager.stop();
+ 		},
+ 	},
+
+ 	behaviors: ['wx://component-export'],
+ 	export () {
+ 		const _this = this;
+ 		return {
+ 			// 开始录音
+ 			startRecord() {
+ 				_this.start()
+ 			},
+ 			// 结束录音
+ 			stopRecord() {
+ 				_this.stop()
+ 			},
+ 			// 播放录音
+ 			play() {
+ 				_this.play()
+ 			},
+ 			// 重置到 ready 状态
+ 			toReady() {
+ 				_this.setData({
+ 					recordStatus: "ready"
+ 				})
+ 			}
+ 		}
+ 	}
+ })
